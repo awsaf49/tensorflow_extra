524fbcdbffbe2e1ed72aca186b1898d025f13c81
Awsaf <awsaf49@gmail.com>
2022-04-06 19:20:26 +0600

    smelu added

---
+++ tensoflow_extra/activations.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+
+def smelu(x, beta=1.5):
+    """Smooth ReLU (SmeLU): Smooth activations and reproducibility in deep networks, https://arxiv.org/abs/2010.09931
+
+    Args:
+        x : numpy or tensorflow tensor
+        beta (float): smooth value. Defaults to 1.5.
+
+    Returns:
+        tensorflow tensor
+    """
+    x = tf.convert_to_tensor(x)
+    return tf.where(tf.math.abs(x)<=beta, ((x+beta)**2)/(4*beta), tf.nn.relu(x))
